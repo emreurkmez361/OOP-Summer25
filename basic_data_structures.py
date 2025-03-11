8d48d83b5acd38fd94939f5a9eac8c8407849465
emreurkmez361 <emreurkmez2017@gmail.com>
2025-03-11 11:17:29 +0100

    Create basic_data_structures.py

---
+++ basic_data_structures.py
@@ -0,0 +1,8 @@
+my_age = 20 # integer
+pi_value = 3.14 # floating point
+greeting = "hello" # string
+numbers_list = [10, 200, 3] # list
+user_info = ("mykey": "value", "name": "emre"} # dictionary
+coordinates = (12, , 234, 567) # tuple
+unique_letters = ("a", "b"} # set 
+is_python_work = True # boolean
