0c519e9605e9b47ebd9e58753d311bc1ea313b77
emreurkmez361 <emreurkmez2017@gmail.com>
2025-03-13 14:36:33 +0100

    students.py

---
+++ students.py
@@ -0,0 +1,28 @@
+student1 = {
+ 'first_name':'Emre',
+  'last_name':'Urkmez',
+  'index_number' : 35447,
+   'nationality' : 'Turkish',
+   'starting_date' : '13/03/2025',
+   'courses' : ['English', 'Maths']
+}
+
+student2 = {
+    'first_name':'Joe',
+  'last_name':'Biden',
+  'index_number' : 35448,
+   'nationality' : 'American',
+   'starting_date' : '20/03/2025',
+   'courses' : ['English', 'Phycsics']
+}
+
+student3 = {
+'first_name':'Ahmet',
+  'last_name':'Kaya',
+  'index_number' : 35448,
+   'nationality' : 'Turkish',
+   'starting_date' : '21/03/2025',
+   'courses' : ['English', 'Polish']
+}
+
+print(student['first_name'] + " " + student['last_name'] + " - Index Number: " + str(student['index_number']))
