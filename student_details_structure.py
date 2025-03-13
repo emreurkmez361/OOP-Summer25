6b3feb6c3c902f5394b00bf27870559a9a1acb4d
emreurkmez361 <emreurkmez2017@gmail.com>
2025-03-13 14:26:52 +0100

    Create student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,9 @@
+student2 = {
+  'first_name':'Joe',
+  'last_name':'Biden',
+  'index_number' : 35448,
+   'nationality' : 'American',
+   'starting_date' : '20/03/2025',
+   'courses' : ['English', 'Physics']
+}
+print(student2.first_name)
