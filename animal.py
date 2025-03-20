710a85208de3a9dc1c47b24ae68b0a8affb31fea
emreurkmez361 <emreurkmez2017@gmail.com>
2025-03-20 14:07:44 +0100

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,41 @@
+cat = {
+    "name": "Cat",
+    "group": "Mammals",
+    "number_of_legs": 4,
+    "skills": ["climbing", "hunting", "jumping"]
+}
+
+dog = {
+    "name": "Dog",
+    "group": "Mammals",
+    "number_of_legs": 4,
+    "skills": ["running", "swimming", "fetching"]
+}
+
+eagle = {
+    "name": "Eagle",
+    "group": "Birds",
+    "number_of_legs": 2,
+    "skills": ["flying", "hunting", "sharp eyesight"]
+}
+
+fish = {
+    "name": "Fish",
+    "group": "Aquatic",
+    "number_of_legs": 0,
+    "skills": ["swimming", "breathing underwater"]
+}
+
+snake = {
+    "name": "Snake",
+    "group": "Reptiles",
+    "number_of_legs": 0,
+    "skills": ["slithering", "hunting", "camouflage"]
+}
+
+if __name__ == "__main__":
+    print(cat)
+    print(dog)
+    print(eagle)
+    print(fish)
+    print(snake)
